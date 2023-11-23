882e759fd04a40dc9a267b7537baee831b0d9037
AlduinoCalderon <aldocalderon97@gmail.com>
2023-11-22 20:12:06 -0600

    raw queries

--- controller/materiacontroller.js
+++ controller/materiacontroller.js
@@ -1,10 +1,12 @@
 const { response } = require("express");
 //const dbConnection = require('../database/conecta');
 const MateriaModel = require('../models/materia');
+const {QueryTypes} = require('sequelize');
 
 
 const getMaterias = async (req, resp=response) => {
-const materias = await MateriaModel.findAll().catch('Error');
+    const materias = await MateriaModel.sequelize.query("select* from materia", {type:QueryTypes.SELECT});
+// const materias = await MateriaModel.findAll().catch('Error');
     resp.json(materias);
  
 }
@@ -34,25 +36,27 @@ const postMateria = async (req, resp = response) => {
         estadoMateria   : body.estadoMateria,
         semestreMateria : body.semestreMateria
     };
+     
+    
     try{
-        const materia = await MateriaModel.create(
-            materiaParam
-        );
-        resp.json({materia});
+        const materia = await MateriaModel.sequelize.query("insert into materia(nombreMateria, estadoMateria, semestreMateria) values(:paramNombre, :paramEstado, :paramSemestre)", {replacements:{
+            paramNombre : materiaParam.nombreMateria,
+            paramEstado : materiaParam.estadoMateria,
+            paramSemestre : materiaParam.semestreMateria
+        }});
+    //    const materia = await MateriaModel.create(materiaParam);
+    
+        resp.json({ Mensaje: "Inserción realizada.",
+                    Nombre : materia[0],
+                    Estado : materia[1],
+                    Semestre : materia[2],
+                    });
+    //    resp.json(materia);
     }
     catch(error){
         console.log(error);
-        resp.status(500).json(
-            {mensaje: "Error en el servidor"}
-            );
+        resp.status(500).json({mensaje: "Error en el servidor"});
     }
-    /*
-    resp.json({
-        respuesta:true,
-        mensaje: 'Llamada a post - insertar',
-        body
-    });
-    */
 }
 const putMateria = async (req, resp = response) => {
    const {cve} = req.params;
