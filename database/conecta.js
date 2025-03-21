f7bef200b7b287a953804abe3d5d784248d85ea2
AlduinoCalderon <alduinocalderon@gmail.com>
2025-03-21 14:46:45 +0900

    Feat: MariaDB

--- database/conecta.js
+++ database/conecta.js
@@ -5,9 +5,14 @@ const dbConn = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
+  
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: 'mariadb',
+    dialectOptions: {
+      connectTimeout: 10000,
+      allowPublicKeyRetrieval: true
+    }
   }
 );
 
